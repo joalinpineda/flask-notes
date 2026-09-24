fad84360f3eec8bb6e04dfab8756adf16ef77271
joalin <joalin.pineda@gmail.com>
2026-09-24 13:23:30 -0600

    added db config and queries for main  list, view and edit

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from config import Config
 from notes.routes import notes_bp
+from models import db
 
 def create_app()-> Flask:
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.register_blueprint(notes_bp)    
     return app
 
